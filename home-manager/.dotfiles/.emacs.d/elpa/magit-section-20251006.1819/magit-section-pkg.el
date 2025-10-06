a18e63c7754fdab2bdae9392721ae17cb692af61
ducksword <Diabotek@proton.me>
2025-10-06 18:30:45 -0400

    test emacs home.file

---
+++ home-manager/.dotfiles/.emacs.d/elpa/magit-section-20251006.1819/magit-section-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit-section" "20251006.1819"
+  "Sections for read-only buffers."
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.1")
+    (llama    "1.0")
+    (seq      "2.24"))
+  :url "https://github.com/magit/magit"
+  :commit "424f094aaac4b3f01a26fffff87cfdb30a9864a1"
+  :revdesc "424f094aaac4"
+  :keywords '("tools")
+  :authors '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")))
